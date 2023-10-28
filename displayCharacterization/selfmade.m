372aa39edb3a6903651d2bcd6470395b133332d5
da5nsy <dannygarside@outlook.com>
2023-10-28 13:37:05 -0400

    update code

---
+++ displayCharacterization/selfmade.m
@@ -0,0 +1,40 @@
+clear, clc, close all
+
+retval = PR655init('COM7');
+S_SPD = [380,1,401];
+
+steps = 0:15:255;
+
+figure, hold on
+for j = 1:3
+    for i = 1:length(steps)
+        im = zeros(1000,1000,3);
+        im(:,:,j) = ones(1000)*steps(i);
+        imshow(uint8(im))
+        drawnow        
+        % SPD = PR670measspd(S_SPD);
+        SPD(:,i,j) = PR655measspd(S_SPD);
+    end
+end
+
+figure, hold on
+for i = 1:length(steps)
+    im = ones(1000,1000,3)*steps(i);
+    imshow(uint8(im))
+    drawnow
+    % SPD = PR670measspd(S_SPD);
+    SPD(:,i,4) = PR655measspd(S_SPD);
+end
+
+%%
+cols = {'r','g','b','k'};
+figure, hold on
+for i = 1:4
+    plot(SPD(:,:,i),'Color',cols{i})
+end
+
+%%
+
+save(['SpectralMeasurement',char(datetime('now','format','yyMMdd-HHmmss'))]);
+
+
--- screenStimuli/imageProcessingPipeline.m
+++ screenStimuli/imageProcessingPipeline.m
@@ -2,8 +2,8 @@ clc, clear, close all
 
 %%
 
-sRGBinput = imread('_MG_9169.tif');
-% figure,imshow(sRGBinput)
+sRGBinput = imread('C:\Users\cege-user\Documents\PMO\screenStimuli\_MG_9169.tif');
+figure,imshow(sRGBinput)
 sRGBreshape = reshape(sRGBinput,[size(sRGBinput,1)*size(sRGBinput,2),size(sRGBinput,3)]);
 
 sRGBlin = SRGBGammaUncorrect(sRGBreshape);
@@ -31,48 +31,67 @@ load T_xyz1931.mat T_xyz1931 S_xyz1931 % Requires PsychToolbox
 
 % Load screen characterization data
 
-screenSPD = readmatrix("../displayCharacterization/measurements/data_init_0.csv");
-S_screenSPD = [384,4,100]; % This is slightly wacky - it should have a value at 380, but it seems to get lost somewhere along the conversion... (TODO Look into this)
+% screenSPD = readmatrix("../displayCharacterization/measurements/data_init_0.csv");
+% S_screenSPD = [384,4,100]; % This is slightly wacky - it should have a value at 380, but it seems to get lost somewhere along the conversion... (TODO Look into this)
+
+load("SpectralMeasurement231027-183237.mat",'SPD','S_SPD');
+screenSPD = SPD;
+S_screenSPD = S_SPD;
+
+screenSPD(:,1,3) = screenSPD(:,2,3);
 
 % Convert to XYZ
 
-screenSPDint = SplineSpd(S_screenSPD,screenSPD,S_xyz1931); % should this be SplineSpd/SplineSrf/SplineRaw (what's the difference?)
-% TODO Flagging that this is an opportunity to get diverging values between the
-% MATLAB vs python pipeline, that I might want to double check
+for i = 1:4
+    screenSPDint(:,:,i) = SplineSpd(S_screenSPD,screenSPD(:,:,i),S_xyz1931); % should this be SplineSpd/SplineSrf/SplineRaw (what's the difference?)
+end
 
 % figure, hold on
-% plot(SToWls(S_screenSPD),screenSPD,'k:');
-% plot(SToWls(S_xyz1931),screenSPDint,'r:');
+% for i = 3%1:4
+%     plot(SToWls(S_screenSPD),screenSPD(:,:,i),'k:');
+%     plot(SToWls(S_xyz1931),screenSPDint(:,:,i),'r:');
+% end
 % xlabel('Wavelength (nm)')
 % ylabel('Transmisson')
 
-screenXYZ = T_xyz1931*screenSPDint;
-screenxyY = XYZToxyY(screenXYZ); % Note, Y is not normalised here
+for i = 1:4
+    screenXYZ(:,:,i) = T_xyz1931*screenSPDint(:,:,i);
+end
+
+% screenXYZ(:,end,3) = screenXYZ(:,end,3) * 1.1616; % attempted correction for blue non-linearity at top of range
+
+for i = 1:4
+    screenxyY(:,:,i) = XYZToxyY(screenXYZ(:,:,i)); % Note, Y is not normalised here
+end
 
 % figure,
 % scatter3(screenXYZ(1,:),screenXYZ(2,:),screenXYZ(3,:))
 % figure,
 % scatter3(screenXYZ(1,37:54),screenXYZ(2,37:54),screenXYZ(3,37:54)) %just blue
 
-scatter3(screenxyY(1,:),screenxyY(2,:),screenxyY(3,:),'k','filled')
+for i = 1:4
+    scatter3(screenxyY(1,:,i),screenxyY(2,:,i),screenxyY(3,:,i),'k','filled')
+end
 
 %% Create conversion matrix
 
-xr = screenxyY(1,18);
-yr = screenxyY(2,18);
-xg = screenxyY(1,36);
-yg = screenxyY(2,36);
-xb = screenxyY(1,54);
-yb = screenxyY(2,54);
-xw = screenxyY(1,72);
-yw = screenxyY(2,72);
+xr = screenxyY(1,18,1);
+yr = screenxyY(2,18,1);
+xg = screenxyY(1,18,2);
+yg = screenxyY(2,18,2);
+xb = screenxyY(1,18,3);
+yb = screenxyY(2,18,3);
+xw = screenxyY(1,18,4);
+yw = screenxyY(2,18,4);
 
 XYZToRGBMat = XYZToRGBMatrix(xr, yr, xg, yg, xb, yb, xw, yw);
 
-% XYZToRGBMat = inv(screenXYZ(:,[18,36,54]));
+% XYZToRGBMat = inv(squeeze(screenXYZ(:,end,[1,2,3]))); 
+% I prefer this way (it's cleaner) but it seems to need a normalising
+% value that I'm missing somewhere (the results come out way too bright)
 
 disp(XYZToRGBMat);
-disp(XYZToRGBMat*screenXYZ(:,[18,36,54]))
+disp(XYZToRGBMat*squeeze(screenXYZ(:,end,[1,2,3])))
 
 % figure,
 % imagesc(M)
@@ -112,11 +131,11 @@ end
 x2 = linspace(0,255,256);
 for i = 1:3
     figure(100+i)
-    p = polyfit(0:15:255,screenxyY_reshape_norm(3,:,i),2);
+    p(i,:) = polyfit(0:15:255,screenxyY_reshape_norm(3,:,i),2);
     if i == 3 % exclude weird datapoint in b
-        p = polyfit(0:15:240,screenxyY_reshape_norm(3,1:end-1,i),2);
+        p(i,:) = polyfit(0:15:225,screenxyY_reshape_norm(3,1:end-2,i),2);
     end
-    y2 = polyval(p,x2);
+    y2 = polyval(p(i,:),x2);
     plot(x2,y2,...
         '--','Color',cols{i},'LineWidth',2)
     LUT(:,i) = y2;
@@ -132,6 +151,7 @@ XYZrequested = [0.5,0.5,0.5];
 
 for i = 1:3
     [~,minloc(i)] = min(abs(XYZrequested(i) - LUT(:,i)));
+    minloc(i) = minloc(i)-1; % to correct for indexing starting at 1
     % disp(minloc(i))
     figure(100+i)
     xline(minloc(i))
@@ -146,6 +166,7 @@ for i = 1:size(RGBlin,2)
         [~,gunVals(j,i)] = min(abs(RGBlin(j,i) - LUT(:,j)));
     end
 end
+gunVals = gunVals-1; % to correct for indexing starting at 1
 
 for i = 1:3
     figure,
